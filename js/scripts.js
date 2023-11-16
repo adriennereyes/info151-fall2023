38e11d49ec711fbf3276eb4633ce6f188e1edc76
Adrienne Estrella <60564656+adriennereyes@users.noreply.github.com>
2023-11-15 21:10:28 -0500

    Had to include toggling sign in and up btns for mobile view

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,20 @@
 var mainNav = document.getElementById("mainNav");
 var burgerBtn = document.getElementById("burgerBtn")
+var signUpInBtns = document.getElementById("sign-in-up-btns-container")
 var navOpen = false;
+var signUpInBtnsShow = false;
 
 function showNav() {
     if (!navOpen) {
         mainNav.classList.add("showMainNav");
         navOpen = true;
+        signUpInBtns.classList.add("showBtns");
+        signUpInBtnsShow = true;
     } else {
         mainNav.classList.remove("showMainNav");
         navOpen = false;
+        signUpInBtns.classList.remove("showBtns");
+        signUpInBtnsShow = false;
     }
 }
 
