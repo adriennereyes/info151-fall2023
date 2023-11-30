6d5d734f3aecc08c4cffbe46143a423fdaf16835
Adrienne Estrella <60564656+adriennereyes@users.noreply.github.com>
2023-11-30 13:57:03 -0500

    Week 8/9, completed sign in modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
+// Nav
 var mainNav = document.getElementById("mainNav");
 var burgerBtn = document.getElementById("burgerBtn")
 var signUpInBtns = document.getElementById("sign-in-up-btns-container")
 var navOpen = false;
 var signUpInBtnsShow = false;
+
+var burgerIcon = document.querySelector("#burger");
+var burgerCloseIcon = document.querySelector("#burger-close");
 
 function showNav() {
     if (!navOpen) {
@@ -10,12 +14,34 @@ function showNav() {
         navOpen = true;
         signUpInBtns.classList.add("showBtns");
         signUpInBtnsShow = true;
+        burgerCloseIcon.classList.remove("hide-burger-close");
+        burgerIcon.classList.add("hide-burger");
     } else {
         mainNav.classList.remove("showMainNav");
         navOpen = false;
         signUpInBtns.classList.remove("showBtns");
         signUpInBtnsShow = false;
+        burgerCloseIcon.classList.add("hide-burger-close");
+        burgerIcon.classList.remove("hide-burger");
     }
 }
 
 burgerBtn.addEventListener("click", showNav);
+
+// Sign In Modal
+var signInBtn = document.getElementById("sign-in-btn");
+var signInModal = document.getElementById("sign-in-modal");
+var closeBtnSignIn = document.getElementById("close-btn-sign-in");
+
+function showSignInModal() {
+    signInModal.classList.add("show-sign-in-modal");
+    mainNav.classList.remove("showMainNav");
+    signUpInBtns.classList.remove("showBtns");
+}
+
+function closeSignInModal() {
+    signInModal.classList.remove("show-sign-in-modal");
+}
+
+signInBtn.addEventListener("click", showSignInModal);
+closeBtnSignIn.addEventListener("click", closeSignInModal);
